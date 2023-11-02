b78bd100569d6a65149984cd94e65e0c0da319d3
Ryan Barfield <cadium828@gmail.com>
2023-11-01 20:51:06 -0500

    added necessary descripions to the describe.js

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -18,6 +18,7 @@ let number = 1;
 
 number++;
 
+
 if (number >= 2) {
   console.log('yes');
 }
@@ -26,10 +27,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We declare a variable with the String of Dane
+// We create a conditional that determines if the name variable equals the string Mary, which it doesnt, so the else block is ran
+// Since the boolean equated to false, the else block would be executed
+// console.log would execute the string 'How do you do'
 
 //CODE
 /*
@@ -45,10 +46,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// On line 56 we declare a variable called secret and leave it as undefined
+// On line 58 we declare a variable called code and set it equal to the integer of 123
+// On line 60 we execute a boolean function to see if code is equal to 123, which would return true
+// Because Line 60 was true, we set secret equal to the string super and we multiple code by 2 (123 * 2)
+// we then ask if code(123 * 2 = 246 )  is greater than 250, which it isnt, so we print out the secret which is super (not duper :( ) 
 
 //CODE
 /*
@@ -70,10 +72,16 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We declare the variables isStudent to true, age to the intger 34, and zip to the integer of 55407
+// Then we enter a conditional 
+// The first if block checks if isStudent strictly equals true AND zip > 80000 
+//          isStudent is true and zip = 55407 which would make this block false so the console.log would be skipped
+// Next it checks if isStudent = false or age < 30
+//            isStudent is True and age = 34, which would make this block false so the console.log would be skipped
+// Next we check if isStudent = true, which it does, so console.log "Welcome to Prime!" would be executed
+// Lastly, the last else block would not be executed because it would 'break' during the Welcome to prime portion
+
+
 
 //CODE
 /*
@@ -129,11 +137,15 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
-/*
+/*  
+// FIX - colorOne in the description is saying it should be set to blue and colorTwo should be red
+// Change the values of colorOne and colorTwo
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
+// FIX - description says we should change colorOne and colorTwo to purple, however we are only changing colorOne
+// add in colorTwo = 'purple';
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -146,6 +158,9 @@ if (mix === true) {
 /*
 let temp = 40;
 const time = 4;
+
+// FIX In the conditional provided || denotes 'or' , however we need both
+// put && instead of ||
 
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
@@ -160,10 +175,15 @@ if (temp > 39 || time >= 4) {
 let age = 21;
 const minAge = 21;
 
+// FIX we're checking if age is greater than or equal to 21, which it is, we need to change the console.log to be enter instead of no entry
+// Another soultion would be to swithc the if statement around and say if (age <= minAge) or if (age >=  minAge) {enter} else {no enter}
+
 if(minAge <= age) {
   console.log('no entry');
 } else {
   console.log('enter');
 }
 */
+
+
 
